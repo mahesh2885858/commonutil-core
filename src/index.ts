fef5c126f80cb6e71cf4c3c64b742b984d35f8c5
maheshbabu <mahesh2885858@gmail.com>
2025-03-27 08:41:38 +0530

    Add input validation to uCFirst function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 /**
  * Return the text with first letter converted to uppercase.
  * Keep in mind that It will trim the string.
- * @param {string} text - text to process
- * @returns {string} - text with first letter uppercased and trimmed.
+ * @param {string} text  text to process
+ * @returns {string} text with first letter uppercased and trimmed.
+ * @throws throws error if not provided a valid string.
  */
 
 export const uCFirst = (text: string): string => {
+  if (!text.trim()) throw "String not provided";
+  if (typeof text !== "string") throw "Not a string";
   const letters = text.trim().split("");
   letters[0] = letters[0].toUpperCase();
   return letters.join("");

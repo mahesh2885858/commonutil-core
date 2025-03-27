cef5260a69daed2719139bc9488416b2391d57ea
maheshbabu <mahesh2885858@gmail.com>
2025-03-27 08:54:47 +0530

    Add getDigits method to extract numeric chars from string

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,22 @@ export const uCFirst = (text: string): string => {
   letters[0] = letters[0].toUpperCase();
   return letters.join("");
 };
+
+/**
+ * Return only digits from the given string.
+ * @param {string} text  text to process
+ * @returns {string} digits from the string.
+ * @throws throws error if not provided a valid string.
+ */
+export const getDigits = (text: string): string => {
+  if (!text.trim()) throw "String not provided";
+  if (typeof text !== "string") throw "Not a string";
+  const letters = text.trim().split("");
+  let digits = "";
+  letters.forEach((letter) => {
+    if (!isNaN(parseInt(letter))) {
+      digits = digits + letter;
+    }
+  });
+  return digits.trim();
+};
